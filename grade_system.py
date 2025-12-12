82c47f167fbee6d1e03e309611b9815ea8373d45
Ritu Tambe <ritutambe2006@gmail.com>
2025-12-12 13:33:53 +0530

    Add files via upload
    Add grade system initial project

---
+++ grade_system.py
@@ -0,0 +1,13 @@
+marks=float(input("Enter your total marks:"))
+if marks>=90:
+    grade="A+"
+elif marks>=70:
+    grade="A"
+elif marks>=60:
+    grade="B"
+elif marks>=50:
+    grade="C"
+else:
+    grade="D"
+print("Your grade is:",grade)
+    
